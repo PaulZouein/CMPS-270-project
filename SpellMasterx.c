3e01a9f4700bf2ea2445c3de66d81f9a152da149
JadElMurr <137875421+JadElMurr@users.noreply.github.com>
2023-11-15 12:46:02 +0200

    Update SpellMasterx.c

--- SpellMasterx.c
+++ SpellMasterx.c
@@ -80,5 +80,143 @@ void set_alarm(int seconds)
     alarm(seconds); // Set the alarm to go off after the specified seconds
 }
 
+void playGame(char spells[MAX_SPELLS][MAX_LENGTH], int spellCount, int difficulty, const char* playerName) 
+{
+    // Variables to keep track of game state
+    int playerTurn = 1; // 1 for player, 0 for computer
+    char lastSpell[MAX_LENGTH] = {0};
+    int usedSpells[MAX_SPELLS] = {0}; // To mark spells that have been used
+    int timeLimit;
+    
+    // Set time limit based on difficulty
+    switch (difficulty) 
+    {
+        case 1: timeLimit = 30; 
+            printf("You have chosen Easy difficulty. You have 30 seconds per turn. Hurry up!\n"); 
+                break; // Easy - 30 seconds
 
+        case 2: timeLimit = 20; 
+            printf("You have chosen Medium difficulty. You have 20 seconds per turn. Hurry up!\n");
+                 break; // Medium - 20 seconds
+
+        case 3: timeLimit = 10; 
+            printf("You have chosen Hard difficulty. You have 10 seconds per turn. Hurry up!\n"); 
+                break; // Hard - 10 seconds
+
+        default: timeLimit = 30; 
+            printf("Invalid input, by default you'll play on Easy difficulty. You have 30 seconds per turn. Hurry up!\n");
+                break; // Default to easy if invalid input - 30 seconds
+    }
+    
+    // Seed the random number generator
+    srand((unsigned int)time(NULL));
+    
+    // Set up signal handler for the alarm
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &handle_alarm;
+    sigaction(SIGALRM, &sa, NULL);
+
+    // Game loop
+    while (1) 
+    {
+        char currentSpell[MAX_LENGTH];
+        
+        // Reset timeout flag
+        timeout_flag = 0;
+        
+        // Player's turn
+        if (playerTurn) 
+        {
+            printf("Your turn, enter a spell: ");
+            fflush(stdout);  // Make sure the prompt is printed immediately
+            
+            // Set an alarm to go off after the specified time limit
+            alarm(timeLimit);
+            
+            // Get player's spell
+            if (scanf("%49s", currentSpell) <= 0 || timeout_flag) 
+            {
+                if(timeout_flag) 
+                {
+                    printf("%s, you lose! Computer wins!\n", playerName);
+                } 
+                
+                else 
+                {
+                    printf("%s, you lose! Computer wins!\n", playerName);
+                }
+
+                break;  // Exit the game loop if input fails or time is up
+            }
+            
+            // Cancel the alarm if input was successful
+            alarm(0);
+
+        int spellIndex = findSpellIndex(spells, spellCount, currentSpell);
+
+       
+    if(spellIndex == -1 || usedSpells[spellIndex] || (lastSpell[0] != '\0' && !isValidSpell(lastSpell[strlen(lastSpell) - 1], currentSpell))) {
+        printf("%s, invalid or repeated spell or wrong starting letter. You lose! Computer wins!\n", playerName);
+        break;
+    }
+
+    // Mark the spell as used
+    usedSpells[spellIndex] = 1;
+
+
+               // Check if the spell exists in the spell list and is valid
+        if((findSpellIndex(spells, spellCount, currentSpell) == -1) || (lastSpell[0] != '\0' && !isValidSpell(lastSpell[strlen(lastSpell) - 1], currentSpell))) 
+        {
+         printf("%s, First spell letter is wrong. You lose! Computer wins!\n", playerName);
+         break;
+        }
+            
+            // Check the player's spell (last letter validation)
+        if(lastSpell[0] != '\0' && !isValidSpell(lastSpell[strlen(lastSpell) - 1], currentSpell)) 
+        {
+            printf("Invalid spell. You lose!\n");
+            break;
+        }
+            
+        } 
+        else 
+        {
+            // Computer's turn 
+            int found = 0;
+            for (int i = 0; i < spellCount && !found; i++) 
+            {
+                if (!usedSpells[i] && isValidSpell(lastSpell[strlen(lastSpell) - 1], spells[i])) 
+                {
+                    strcpy(currentSpell, spells[i]);
+                    printf("Computer's spell: %s\n", currentSpell);
+                    found = 1;
+                }
+            }
+
+    int spellIndex = findSpellIndex(spells, spellCount, currentSpell);
+
+    if (usedSpells[spellIndex]) 
+    {
+        printf("Computer cannot find a valid spell. Computer loses!\n");
+        break;
+    }
+
+    // Mark the spell as used
+    usedSpells[spellIndex] = 1;
+
+
+            if (!found) 
+            {
+                printf("Computer cannot find a valid spell. Computer loses!\n");
+                break;
+            }
+    }
+         
+        // Update game state
+        strcpy(lastSpell, currentSpell);
+        usedSpells[findSpellIndex(spells, spellCount, currentSpell)] = 1; // Mark spell as used
+        playerTurn = !playerTurn; // Switch turns
+    }
+}
 
